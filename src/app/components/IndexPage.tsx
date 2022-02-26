c21cd101e2d8aa1eae54ce34537cb07d163e7b4e
tkugimot <toshimitsu_kugimoto@yahoo.co.jp>
2022-02-26 16:18:21 +0900

    Add tailwind

--- src/app/components/IndexPage.tsx
+++ src/app/components/IndexPage.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export const IndexPage = () => {
     return (
         <div>
-            <h1>Hello World :)</h1>
+            <h1 className="text-3xl font-bold underline">
+                Hello world :)
+            </h1>
         </div>
     );
 }
